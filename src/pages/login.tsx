79e6ea1e0a799eb2b67eca1e97acab3cccc29cc6
Eetu Petänen <eetu.petanen@gmail.com>
2026-05-09 14:34:45 +0300

    Fix typescript errors

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,23 @@
-import { getProviders, signIn } from 'next-auth/react';
+import {
+  getProviders,
+  signIn,
+  type ClientSafeProvider,
+  type LiteralUnion
+} from 'next-auth/react';
+import type { BuiltInProviderType } from 'next-auth/providers/index';
 
-function Login({ providers }) {
+type Props = {
+  providers: Record<LiteralUnion<BuiltInProviderType>, ClientSafeProvider>;
+};
+
+function Login({ providers }: Props) {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
             className="rounded-full bg-[#18D860] p-5 text-white"
-            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+            onClick={() => void signIn(provider.id, { callbackUrl: '/' })}
           >
             Login with {provider.name}
           </button>
@@ -21,7 +31,6 @@ export default Login;
 
 export async function getServerSideProps() {
   const providers = await getProviders();
-
   return {
     props: {
       providers
